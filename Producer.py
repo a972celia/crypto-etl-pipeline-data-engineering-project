678f5a7061812311ce6a6c191cc87f372fc43170
a972celia <celiakuo0804@gmail.com>
2023-10-17 23:10:27 +0800

    add environment variable

--- Producer.py
+++ Producer.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from kafka import KafkaProducer
 from time import sleep
 import json
@@ -14,10 +15,14 @@ def get_crypto_price(crypto):
         print(f"Error retrieving price for {crypto}: {e}")
         return None
 
+
+# Fetch the Kafka broker URL from the environment variables
+KAFKA_BROKER_URL = os.environ.get("KAFKA_BROKER_URL", "default_kafka_service:9092")
+
 # Create a Kafka producer instance
 producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',
-    value_serializer=lambda v: str(v).encode('utf-8')
+    bootstrap_servers = KAFKA_BROKER_URL,
+    value_serializer = lambda v: str(v).encode('utf-8')
     # Add any additional Kafka producer configurations here
 )
 
@@ -28,7 +33,14 @@ try:
             if price is not None:
                 producer.send(crypto, value=price)
         sleep(10)
+        
 except KeyboardInterrupt:
     print("Shutting down...")
+except requests.RequestException as e:
+    print(f"Network-related error occurred: {e}")
+except KafkaError as e:
+    print(f"Kafka error occurred: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
 finally:
     producer.close()
